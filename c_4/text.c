886b719da032fd43e8c9f806513422b87f3371e8
zaqmlp523 <128678158+zaqmlp523@users.noreply.github.com>
2023-03-23 16:11:30 +0900

    Add files via upload

---
+++ c_4/text.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{ 
+    int x,y,re;
+    int n1=0, n2=0, n3=0;
+    scanf("%d %d", &x, &y);
+
+    n1 =y%10;
+    n2 = x *(y/10%10);
+    n3 = x *(y/100);
+
+    printf("%d\n" , n1);
+    printf("%d\n")
+
+
+
+  
+
+
+
+
+
+
+}
+
